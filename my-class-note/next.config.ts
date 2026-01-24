d2554a89d0e6dafe029b33fae3f2a4942510506e
Ling Chang <127913579+hunlin99@users.noreply.github.com>
2026-01-24 05:53:55 +0000

    add basePath and assetPrefix to fix the link issue

--- my-class-note/next.config.ts
+++ my-class-note/next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // This is the key line
+  basePath: '/Class-Note',
+  assetPrefix: '/Class-Note',
+  images: {
+    unoptimized: true, // GitHub Pages doesn't support Next.js Image Optimization
+  },
+  // If your repo is NOT hunlin99.github.io, you need the line below:
+  // basePath: '/Class-Note',
 };
 
 export default nextConfig;
