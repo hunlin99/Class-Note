adc0ac1d61b98d728e64a6d45bebd90ac9aa29d5
Ling Chang <127913579+hunlin99@users.noreply.github.com>
2026-01-30 08:35:06 +0000

    Updates

--- _next/static/cL7U-Y-j_Fu9ymi_46zC5/_buildManifest.js
+++ _next/static/cL7U-Y-j_Fu9ymi_46zC5/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Class-Note//_next/:path+",
+        "destination": "/Class-Note/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
